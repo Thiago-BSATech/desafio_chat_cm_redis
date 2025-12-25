21b54c0fb9e98b0ba21f841009503579ee35c0d5
Thiago-BSATech <thiago.mendonca@bsatech.io>
2025-12-25 15:29:05 -0300

    feat: create ws for public chat broadcast with redis

--- app/db.py
+++ app/db.py
@@ -1,27 +1,16 @@
-import redis
+import redis.asyncio as redis
 
 client = None
 
 def get_db():
-
     global client
 
     if client is None:
-        try:
-            client = redis.Redis(
-                host='localhost', 
-                port=6379, 
-                decode_responses=True
-            )
+        client = redis.Redis(
+            host='localhost',
+            port=6379,
+            decode_responses=True
+        )
+        print("Conectado com Redis")
 
-            print("Conectado com Redis")
-
-            return client
-            
-        except Exception as e:
-            raise Exception('AN ERROR HAS OCURRED', e)
-        
-
-# test = get_db()
-
-# test.set('foo', 'test----test')
+    return client
