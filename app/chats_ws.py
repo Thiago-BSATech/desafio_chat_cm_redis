89c5d26214f54e287001d8c8aa2d40055476225f
Thiago-BSATech <thiago.mendonca@bsatech.io>
2025-12-25 19:37:05 -0300

    feat: create group chat and delete unused file

--- app/chats_ws.py
+++ app/chats_ws.py
@@ -20,10 +20,16 @@ async def chat_ws(ws: WebSocket):
 
     try:
         no_response = True
-        responses = ["/global", "/private"]
+        responses = ["/global", "/private", "/group"]
         print("ESCOLHA UM CHAT PARA ENTRAR: ")
         print(responses)
-        print("parametros: username(obrigatório) e target(apenas para o private)")
+        print(
+            """
+                parametros: username(obrigatório para todos os chats), 
+                group: indique o grupo no qual quer se juntar (apenas para o group),
+                target: indique a pessoa na qual você quer conversar(apenas para o private)
+            """
+        )
         
 
         while no_response:
@@ -89,6 +95,50 @@ async def chat_ws(ws: WebSocket):
 
             chat_key = f"private:{users[0]}:{users[1]}:c"
             pub_key = f"private:{users[0]}:{users[1]}:pb"
+
+            history = await db.lrange(chat_key, -50, -1)
+            await history_message(history, ws)
+
+            await pubsub.subscribe(pub_key)
+
+            while True:
+                message = await pubsub.get_message(ignore_subscribe_messages=True, timeout=0.01)
+
+                if message:
+                    await ws.send_text(message["data"])
+
+                try:
+                    text = await asyncio.wait_for(ws.receive_text(), timeout=0.01)
+                except asyncio.TimeoutError:
+                    continue
+
+                data = json.dumps({
+                    "user": username,
+                    "text": text,
+                    "time": datetime.datetime.utcnow().strftime("%H:%M")
+                })
+
+                await redis_pusher(db, chat_key, pub_key, data)
+                
+
+        except WebSocketDisconnect:
+            pass
+        finally:
+            await pubsub.unsubscribe(pub_key)
+            await pubsub.close()
+
+
+    elif response == "/group":
+        try:
+
+            group = ws.query_params.get("group")
+            if not group:
+                print("informe o group nos parametros")
+                await ws.close(code=1008)
+                return
+
+            chat_key = f"group_chat_c:{group}"
+            pub_key = f"group_chat_pb:{group}"
 
             history = await db.lrange(chat_key, -50, -1)
             await history_message(history, ws)
